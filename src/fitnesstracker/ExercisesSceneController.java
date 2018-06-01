f40cd4213d10dfdcbb35a074910a9b3a4a31f861
David Arico <davidarico@me.com>
2018-06-01 02:38:07 -0700

    Add files via upload

--- src/fitnesstracker/ExercisesSceneController.java
+++ src/fitnesstracker/ExercisesSceneController.java
@@ -5,9 +5,13 @@
  */
 package fitnesstracker;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -19,6 +23,7 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 
 /**
@@ -181,7 +186,9 @@ public class ExercisesSceneController {
         Button submitButton = new Button("Submit");
         submitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
-                System.out.println("test");
+                DataInput.EnterExercise("exercise", Integer.parseInt(userCalorieInput.getText()));
+                System.out.println(userCalorieInput.getText());
+                exerciseStage.close();
             }
         });
         Label emptySpace3 = new Label(" ");
